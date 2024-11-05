ab947e85823b9cd845febb9734de3aa9b41844c9
Phineas Kibbey <126810951+phineas-dev@users.noreply.github.com>
2024-11-05 23:11:15 +0000

    add biome and fix lint issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'http://localhost:4321/',
+  integrations: [mdx(), sitemap()],
+  devToolbar: {
+    enabled: false,
+  },
 });

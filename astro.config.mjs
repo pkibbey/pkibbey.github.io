3aeda74925f6919db01148922e909c007312be06
Phineas Kibbey <126810951+phineas-dev@users.noreply.github.com>
2024-11-06 00:06:07 +0000

    remove unused package

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://phineas-dev.github.io',
-  base: 'blog',
+  site: 'https://phineas-dev.github.io/blog',
   integrations: [mdx(), sitemap()],
   devToolbar: {
     enabled: false,

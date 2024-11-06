30d816842c1f6d18aeb1953f85690958f5c0a2da
Phineas Kibbey <126810951+phineas-dev@users.noreply.github.com>
2024-11-06 00:39:38 +0000

    Update the home page

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Phineas Kibbey';
+export const SITE_DESCRIPTION = 'Personal blog of Phineas Kibbey';

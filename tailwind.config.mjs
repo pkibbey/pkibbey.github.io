e9e5e4b3d2b391e0b2d2dcb3ff626d7ec51029af
Phineas Kibbey <126810951+phineas-dev@users.noreply.github.com>
2024-11-13 19:20:00 -0800

    add work sections

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      flex: {
+        '2': '2 2 0%',
+        '3': '3 3 0%'
+      }
+    },
   },
   plugins: [],
 };

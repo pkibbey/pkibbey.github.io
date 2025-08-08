ad65ed169e11ecb39f1335f87d536f29035cab64
Phineas Kibbey <phineas.kibbey@gmail.com>
2025-08-08 20:15:49 +0200

    update header

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,12 +1,12 @@
 export default function Hero() {
   return (
-    <section className="container max-w-2xl mx-auto px-4">
-      <h1 className="typography-heading-1 mb-3">Phineas Kibbey</h1>
+    <div className="container max-w-2xl mx-auto px-4">
+      <h2 className="typography-heading-1 mb-3">Phineas Kibbey</h2>
       <p className="typography-caption mb-4">
         My story is one of continuous adaptation and learning, from
         co-founding one of the UK's first social networks to architecting
         solutions for modern smart homes.
       </p>
-    </section>
+    </div>
   )
 }
